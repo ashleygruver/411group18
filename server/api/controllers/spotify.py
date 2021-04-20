34229fe62b4ed0a4b24ae5424bf56254bf46bd56
Jin Young Bang <jybang@bu.edu>
2021-04-20 17:34:26 -0400

    Created api that fetches generated playlist based off of user_id

--- server/api/controllers/spotify.py
+++ server/api/controllers/spotify.py
@@ -1,7 +1,9 @@
 from flask import (Blueprint, make_response, jsonify, request)
+from api import mongo
 import requests
 
 spotify = Blueprint("spotify", __name__)  # initialize blueprint
+playlist_db = mongo.db.playlists
 
 
 @spotify.route("/api/get-playlists", methods=["GET"])
@@ -37,8 +39,9 @@ def get_all_album_covers(playlist_id):
         response = requests.request("GET", url, headers=headers, data=payload)
         response = response.json()
         tracks = response["tracks"]["items"]
-        
-        album_urls= [tracks[i]["track"]["album"]["images"][1]["url"] for i in range(len(tracks))]
+
+        album_urls = [tracks[i]["track"]["album"]["images"][1]["url"]
+                      for i in range(len(tracks))]
         response = {
             "albumUrls": album_urls
         }
@@ -47,3 +50,46 @@ def get_all_album_covers(playlist_id):
     except Exception as e:
         print(e)
 
+
+# This is a function
+@spotify.route("/api/view-generated-playlists", methods=["GET"])
+def view_generated_playlist():
+    url = "https://api.spotify.com/v1/me"
+    access_token = request.cookies.get('access_token')
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer ' + access_token
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response = response.json()
+    spotify_user_id = response["id"]
+    user_exists = (playlist_db.find({"userId": spotify_user_id}).count() != 0)
+    generated_playlists = []
+
+    if not user_exists:
+        try:
+            user_data = {
+                "userId": spotify_user_id,
+                "playlists": []
+            }
+            playlist_db.insert_one(user_data)
+
+        except Exception as e:
+            print(e)
+
+    else:
+        data = playlist_db.find_one({"userId": spotify_user_id})
+        generated_playlists = data["playlists"]
+
+    response_object = {
+        "generatedPlaylists": generated_playlists
+    }
+
+    return make_response(response_object, 200)
+
+@spotify.route("/api/add-generated-playlists/<playlist_id>", methods=["POST"])
+def add_generated_playlist(playlist_id):
+    # add_generated_playlist(user_id):
+    #     - if user_id doesn’t exist, create a user_id field with that generated playlist
+    #     - else, just add/append on that specific user_id field.
