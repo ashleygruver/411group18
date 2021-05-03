4cccb1f44da4822548cab3f31cde980e36764584
Jin Young Bang <jybang@bu.edu>
2021-05-02 21:00:49 -0400

    css changes

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -8,78 +8,86 @@ import querystring from 'querystring';
 
 const Home = () => {
 
-    const [loggedIn, setLoggedIn] = useState(false);
-  
-    const spotifyLogin = () => {
-      const client_id = "1c5183fbe4c5491789c87f8638deb99d";
-      const redirect_uri = "http://localhost:5000/callback";
-      var scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public playlist-read-collaborative';
-      window.open('https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-          response_type: 'code',
-          client_id: client_id,
-          scope: scopes,
-          redirect_uri
-        }), "_self"
-      )
-    }
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            minHeight: '100vh',
-            backgroundImage: `url(${process.env.PUBLIC_URL + '/imgs/listening.png'})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: 'cover',
-        },
-        logo: {
-            display: 'flex',
-            justifyContent: 'center',
-            paddingTop: '50px',
-        },
-        text: {
-            display: 'flex',
-            justifyContent: 'center', //horizontal
-            alignItems: 'center', //vertical
-            lineHeight: '80px', //line spacing
-            letterSpacing: '10px', //letter spacing
-            height: '450px',
-            textAlign: 'center',
-            color: 'white',
-            fontSize: 20,
-        },
-        spotify: {
-            display: 'flex',
-            justifyContent: 'center',
-        },
-        button: {
-            display: 'flex',
-            justifyContent: 'center',
-        }
-    }));
-    
-    const classes = useStyles();
-    return (
-      (
-        <div className={classes.root}>
-            <div className={classes.logo}>
-                <img src='/imgs/Group 11.png' width='13%'></img>
-            </div>
-            <div className={classes.text}>
-                <h1>WHERE <br /> MUSIC MEETS <br /> COLOR.</h1>
-            </div>
-            <div className={classes.spotify}>
-              <img src='/imgs/spotify2.png' width='13%'></img>
-            </div>
-            <div className={classes.button}>
-              <Button onClick={spotifyLogin} variant="contained" color="primary">Login with Spotify</Button>
-            </div>
-            {/* <button className={classes.button}>
-              <img src="/imgs/spotify.png" onClick={spotifyLogin} />
-            </button> */}
-        </div>
-      )
-  
+  const spotifyLogin = () => {
+    const client_id = "1c5183fbe4c5491789c87f8638deb99d";
+    const redirect_uri = "http://localhost:5000/callback";
+    var scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-private playlist-modify-public playlist-read-collaborative';
+    window.open('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: scopes,
+        redirect_uri
+      }), "_self"
     )
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      minHeight: '100vh',
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/imgs/listening.png'})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: 'cover',
+      display: "block",
+      float: "left"
+    },
+    logo: {
+      display: 'flex',
+      justifyContent: 'center',
+      paddingTop: '50px',
+    },
+    text: {
+      display: 'flex',
+      justifyContent: 'center', //horizontal
+      alignItems: 'center', //vertical
+      lineHeight: '80px', //line spacing
+      letterSpacing: '10px', //letter spacing
+      height: '450px',
+      textAlign: 'center',
+      color: 'white',
+      fontSize: 20,
+    },
+    spotify: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    button: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    spotifyLogin: {
+      paddingBottom: "5%"
+    }
+  }));
+
+  const classes = useStyles();
+  return (
+    (
+      <div className={classes.root}>
+        <div className={classes.logo}>
+          <img src='/imgs/Group 11.png' width='13%'></img>
+        </div>
+        <div className={classes.text}>
+          <h1>WHERE <br /> MUSIC MEETS <br /> COLOR.</h1>
+        </div>
+        <div className={classes.spotifyLogin}>
+          <div className={classes.spotify}>
+            <img src='/imgs/spotify2.png' width='13%'></img>
+          </div>
+          <div className={classes.button}>
+            <Button onClick={spotifyLogin} variant="contained" style={{
+              backgroundColor: "#1DB954",
+              color: "#191414",
+              fontWeight: "bold"
+            }}>Login with Spotify</Button>
+          </div>
+        </div>
+      </div>
+    )
+
+  )
 }
 
 export default Home;
