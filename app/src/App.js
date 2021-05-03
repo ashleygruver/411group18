d6dd94ee41056212d17e65a84a73c49f30e844b0
cchukwuj <69560096+cchukwuj@users.noreply.github.com>
2021-05-03 15:02:55 -0400

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,8 @@ import { Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from './components/Home'
 import Test from './components/Test'
 import Playlists from './components/Playlists'
-import GeneratedPlaylists from './components/GeneratedPlaylists'
+import Song from './components/Song'
+
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route exact path="/test" component={Test} />
       <Route exact path="/playlists" component={Playlists} />
-      <Route exact path="/playlists/generated" component={GeneratedPlaylists} />
+      <Route exact path="/song/:id" component={Song} />
+
     </Router>
   );
 }
